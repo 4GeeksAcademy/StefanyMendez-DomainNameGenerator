d57513e1fc8ef58e6349c1bd73b6dd8519e550aa
Stefany María Méndez Salas <128415958+Tefis15@users.noreply.github.com>
2023-06-20 01:15:41 +0000

    Segunda Versión

--- src/app.js
+++ src/app.js
@@ -1,41 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [
+  ".com",
+  ".cr",
+  ".us",
+  ".xyz",
+  ".es",
+  ".co",
+  ".net",
+  ".io",
+  ".ar"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let number = 1;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  domainGeneretor();
-};
-function domainGeneretor() {
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let extension = [
-    ".com",
-    ".cr",
-    ".us",
-    ".xyz",
-    ".es",
-    ".co",
-    ".net",
-    ".io",
-    ".ar"
-  ];
-  let domainTxt = "";
-
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        for (let l = 0; l < extension.length; l++) {
-          domainTxt += pronoun[i] + adj[j] + noun[k] + extension[l] + "\n";
-        }
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        //domainTxt +=
+          console.log(number + "  " + pronoun[i] + adj[j] + noun[k] + extension[l]);
+        number++;
       }
     }
   }
-  const p = document.getElementById("domain");
-  p.innerText = domainTxt;
 }
+
